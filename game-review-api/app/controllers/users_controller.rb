d39e973123cabeb3b65e0b50f119ca43b5e34993
ixnp <enprocopios@gmail.com>
2022-02-03 09:21:55 -0800

    solution

--- game-review-api/app/controllers/users_controller.rb
+++ game-review-api/app/controllers/users_controller.rb
@@ -7,6 +7,22 @@ class UsersController < ApplicationController
     render json: {errors: invalid.record.errors}, status: :unprocessable_entity
  end 
 
+ def update
+   user = User.find(params[:id])
+   user.update!(user_params)
+   render json: user, status: :ok
+ rescue ActiveRecord::RecordInvalid => invalid
+   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+ end 
+
+ def destroy 
+   user = User.find(params[:id])
+   user.destroy
+   head :no_content 
+ rescue ActiveRecord::RecordInvalid => 
+   render json: {errors:  invalid.record.errors}, status: :not_found
+ end 
+
  private
  
  def user_params
